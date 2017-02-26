01aa4ec0433e131d674aa1f5d7e3e939bd9353e4
Florin Mihalache <florin.mihalache@gmail.com>
2017-02-26 23:39:28 +0200

    Sub-requests implemented.

--- jira.rb
+++ jira.rb
@@ -1,9 +1,8 @@
 require 'request'
-require 'request_queue'
 
+# Jira class wrapper that basically runs the script
 class Jira
-
-  attr_accessor :config, :request_queue, :request
+  attr_accessor :config, :request_queue
 
   def initialize
     self.config = {
@@ -11,116 +10,74 @@ class Jira
         default_type: 'Story',
         default_subtask_type: 'Technical task'
     }
-    self.request_queue = RequestQueue.new
   end
 
   def self.run(&block)
-    j = Jira.new
     Jira.new.instance_eval(&block)
-    j.request_queue.run
+  rescue StandardError => e
+    p "ERROR: #{e.message}"
   end
 
   # config related methods
   def user(u)
-    self.config[:user] = u
+    config[:user] = u
   end
 
   def password(p)
-    self.config[:password] = p
+    config[:password] = p
   end
 
   def host(h)
-    self.config[:host] = h
+    config[:host] = h
   end
 
   def api_path(api)
-    self.config[:api_path] = api
+    config[:api_path] = api
   end
 
   def project(p)
-    self.config[:project] = p
+    config[:project] = p
   end
 
   # build request config
   def _get_request_config(res, type)
     {
-        user: self.config[:user],
-        password: self.config[:password],
-        host: self.config[:host],
-        api_path: self.config[:api_path],
-        project: self.config[:project],
-        request_type: type,
-        resource: res
+        user: config[:user],
+        password: config[:password],
+        host: config[:host],
+        api_path: config[:api_path],
+        project: config[:project],
+        http_request_type: type,
+        resource: res,
+        default_task_type: config[:default_task_type],
+        default_subtask_type: config[:default_subtask_type]
     }
-  end
-
-  def _show_result(response, tag, success_msg)
-    if response.success?
-      p "[#{tag}] #{success_msg}"
-    elsif response.timed_out?
-      p "[#{tag}] timeout"
-    elsif response.code == 0
-      p "[#{tag}] #{response.return_message}"
-    else
-      p "[#{tag}] request failed: #{response.code.to_s}"
-      p response.body
-    end
   end
 
   # update command
   def update(key, &block)
+    raise "No update definition for issue #{key}" unless block_given?
+    request_config = _get_request_config('issue/' + key, :put)
+    request = Request.new(:update, request_config)
+    request.key = key
+    request.instance_eval(&block)
+    request.run
 
-    raise Exception.new 'cannot start a new command inside another' unless self.request.nil?
-    raise Exception.new 'no update definition' unless block_given?
-
-    ret = nil
-    request_config = self._get_request_config('issue/' + key, :put)
-
-    self.request = Request.new(request_config)
-    self.instance_eval(&block)
-    response = self.request_queue.add(self.request)
-    self._show_result(response, key, 'update successful')
-    self.request = nil
+    p "Issue #{key} updated successfully"
   end
 
   def create(summary, &block)
-
-    raise Exception.new 'cannot start a new command inside another' unless self.request.nil?
-
     # create new request
-    request_config = self._get_request_config('issue', :post)
-    self.request = Request.new(request_config)
-    self.request.summary summary
-    self.request.project self.config[:project]
+    request_config = _get_request_config('issue', :post)
+    request = Request.new(:create, request_config)
+    request.summary summary
+    request.project config[:project]
 
-    self.instance_eval(&block) if block_given?
-
-    # set default type if empty
-    if !self.request.fields.key?(:type)
-      if self.request.fields.key?(:parent)
-        self.request.type self.config[:default_subtask_type]
-      else
-        self.request.type self.config[:default_type]
-      end
-    end
+    request.instance_eval(&block) if block_given?
 
     # run request
-    response = self.request_queue.add(self.request)
-    if response.success?
-      resp = JSON.parse(response.body)
-      ret = resp['key'] if resp.key?('key')
-    end
+    k = request.run
 
-    self._show_result(response, summary, 'created successfully')
-
-    self.request = nil
-
-    ret
-
+    p "Issue #{k}: #{summary} created successfully"
   end
-
-  def method_missing(name, *args, &block)
-    self.request.send(name, args, &block)
-  end
-
 end
--- request.rb
+++ request.rb
@@ -1,12 +1,13 @@
 require 'json'
+require 'typhoeus'
 
+# Generic Jira request
 class Request
-
   # request config - host, api resource, etc
   attr_accessor :config
 
   # request data to send to server
-  attr_accessor :data
+  attr_accessor :json_data
 
   # request fields that were set
   attr_accessor :fields
@@ -20,10 +21,27 @@ class Request
   # request response
   attr_accessor :response
 
-  def initialize(config = {})
+  # request children
+  attr_accessor :children
+
+  # request type (create, update)
+  attr_accessor :request_type
+
+  # issue key
+  attr_accessor :key
+
+  def initialize(type, config = {})
+    self.request_type = type
     self.config = config
     self.fields = {}
     self.request_parent = nil
+    self.json_data = {}
+    self.children = []
+    self.key = nil
+    init_data_map
+  end
+
+  def init_data_map
     self.data_map = {
         project: 'fields/project/key',
         parent: 'fields/parent/key',
@@ -34,11 +52,10 @@ class Request
         estimate: 'fields/timetracking/originalEstimate',
         remaining: 'fields/timetracking/remainingEstimate'
     }
-    self.data = {}
   end
 
   def to_json
-    JSON.generate(self.data)
+    JSON.generate(json_data)
   end
 
   def _set_xpath(h, xpath, value)
@@ -49,21 +66,96 @@ class Request
     else
       key = xpath[0..pos - 1].to_sym
       h[key] = h[key] || {}
-      self._set_xpath(h[key], xpath[pos + 1..len], value)
+      _set_xpath(h[key], xpath[pos + 1..len], value)
     end
     h
   end
 
+  def respond_to_missing?(method_name, include_private = false)
+    data_map.key?(method_name) || super
+  end
+
   def method_missing(name, *args, &block)
-    if self.data_map.key?(name)
-      self.fields[name] = *args[0]
-      self._set_xpath(self.data, self.data_map[name], *args[0])
+    if data_map.key?(name)
+      fields[name] = *args[0]
+      _set_xpath(json_data, data_map[name], *args[0])
     else
-      raise Exception.new 'Invalid request parameter: ' + name.to_s
+      super
+    end
+  end
+
+  def _error(msg)
+    if request_type == :create
+      raise "Error trying to create ticket #{fields[:summary]}: #{msg}"
+    elsif request_type == :update
+      raise "Error trying to update ticket #{key}: #{msg}"
+    end
+  end
+
+  def run
+    ret = nil
+    _set_default_type
+    url = [config[:host], config[:api_path], config[:resource]].join('/')
+    req = Typhoeus::Request.new(
+        url,
+        ssl_verifypeer: false,
+        method: config[:http_request_type],
+        userpwd: "#{config[:user]}:#{config[:password]}",
+        body: to_json,
+        headers: { 'Content-Type' => 'application/json' }
+    )
+    req.on_complete do |response|
+      if response.success?
+        self.response = JSON.parse(response.body, symbolize_names: true)
+        ret = self.response[:key]
+        self.key = ret if request_type == :create
+        unless children.empty?
+          children.each do |child|
+            child.parent key
+            child.run
+          end
+        end
+      elsif response.timed_out?
+        _error('timeout')
+      elsif response.code.zero?
+        # Could not get an http response, something's wrong.
+        _error(response.return_message)
+      else
+        # Received a non-successful http response.
+        _error('HTTP request failed: ' + response.code.to_s + ' / message: ' + response.body)
+      end
+    end
+
+    req.run
+
+    ret
+  end
+
+  def subtask(summary, &block)
+    raise "Sub-task #{fields[:summary]} cannot have other sub-tasks" unless request_parent.nil?
+    request = Request.new(:create, config)
+    request.config[:http_request_type] = :post
+    request.request_parent = self
+    children.push request
+    request.summary summary
+    request.instance_eval(&block) if block_given?
+  end
+
+  def create(summary, &block)
+    subtask(summary, &block)
+  end
+
+  def _set_default_type
+    # set default type if empty
+    return unless fields.key?(:type)
+    if fields.key?(:parent)
+      type config[:default_subtask_type]
+    else
+      type config[:default_type]
     end
   end
 
   def to_s
-    self.data
+    json_data
   end
 end
